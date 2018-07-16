faa86e73b0231b4819302be9b35494491aa65e4d
Ruger1983 <mailto:aleksvld1983@yandex.ru>
2018-07-16 21:16:21 +0300

    test has been passed, at last...

---
+++ chapter_001/src/test/java/ru/correct/calculate/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.correct.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Aleksei Aizenberg (mailto:aleksvld1983@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Aleks";
+        String expect = "Echo, echo, echo : Aleks";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
+}
