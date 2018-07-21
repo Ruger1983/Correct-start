d5e9f62459dd5b987d55ec68e937393ddf1f8add
Ruger1983 <mailto:aleksvld1983@yandex.ru>
2018-07-21 10:54:18 +0300

    Now the Converter is correct

--- chapter_001/src/test/java/ru/correct/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/correct/converter/ConverterTest.java
@@ -8,28 +8,28 @@ public class ConverterTest {
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToEuro(1);
-        assertThat(result, is(70));
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
     }
 
     @Test
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(1);
-        assertThat(result, is(60));
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
     }
 
     @Test
     public void when1EuroToRubleThen70() {
         Converter converter = new Converter();
-        int result = converter.euroToRuble(70);
-        assertThat(result, is(1));
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
     }
 
     @Test
     public void when1DollarToRubleThen60() {
         Converter converter = new Converter();
-        int result = converter.dollarToRuble(60);
-        assertThat(result, is(1));
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
     }
 }
