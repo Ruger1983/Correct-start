a4113a671118eb3a299ee75d8e10bbe74ec014ae
Ruger1983 <mailto:aleksvld1983@yandex.ru>
2018-07-26 21:47:59 +0300

    Now I can use a ternal operations

---
+++ chapter_001/src/main/java/ru/correct/max/Max.java
@@ -0,0 +1,14 @@
+package ru.correct.max;
+
+/**
+ * @author Aleksey Aizenberg (mailto:aleksvld1983@yandex.ru)
+ * @version &Id$
+ * @since 0.1
+ */
+
+public class Max {
+    public int max(int first, int second) {
+        int count = first > second ? first : second;
+                return count;
+    }
+}
