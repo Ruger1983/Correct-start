61c5be051bfbcba4b9ed9b057eeaf8c68029dd33
Ruger1983 <mailto:aleksvld1983@yandex.ru>
2018-08-14 18:24:57 +0300

    Some changes in the code. Now it more short

--- chapter_001/src/main/java/ru/correct/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/correct/array/MatrixCheck.java
@@ -9,7 +9,6 @@ package ru.correct.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        boolean temp3 = true, temp4 = true;
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data.length; j++) {
                 if (i == j) {
@@ -17,20 +16,13 @@ public class MatrixCheck {
                     if (!temp) {
                         result = false;
                     }
-                    temp3 = temp;
                 }
                 if (j == data.length - i) {
                     boolean temp2 = data[i][j];
                     if (!temp2) {
                         result = false;
                     }
-                    temp4 = temp2;
                 }
-            }
-        }
-        if (data.length % 2 != 0) {
-            if (temp3 != temp4) {
-                result = false;
             }
         }
         return result;
